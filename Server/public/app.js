2d70cc59fc85be32e00dd3b8c85abb4f332e54e6
adnanioricce <adnanioricce@outlook.com>
2022-06-21 20:47:30 -0300

    centering the div on index.html

--- Server/public/app.js
+++ Server/public/app.js
@@ -1,29 +1,27 @@
 function Api(baseUrl) {
     const ENDPOINTS = {
-        GetWord:`${baseUrl}/getWord`
+        GetWord: 'getWord'
     }
-    return {
-        getRandomWord: async () => {
-            const response = await fetch(ENDPOINTS.GetWord)
-            const value = await response.json()
-            return value.word;
-        }
+    
+    this.getRandomWord = async () => {
+        const response = await fetch(ENDPOINTS.GetWord)
+        const value = await response.json()
+        return value.word;
+    }    
+}
+function vm() {
+    const onGenerateWord = async (vm,api) => {    
+        const word = await api.getRandomWord()
+        const wordContainer = vm.getWordContainer()
+        wordContainer.innerText = word
     }
-}
-const onGenerateWord = async (vm,api) => {    
-    const word = await api.getRandomWord()
-    const wordContainer = vm.getWordContainer()
-    wordContainer.innerText = word
-}
-function vm(params) {
     this.getWordContainer = () => document.getElementById("wordContainer")    
     this.getGenerateWordBtn = () => document.getElementById("generateWordBtn")    
-    this.api = Api("http://localhost:8080")
+    this.api = new Api('')
     this.init = () => {
         const btn = this.getGenerateWordBtn()
         btn.onclick = (e) => onGenerateWord(this,this.api)
     }        
-    this.init()
-    console.log("vm initialized")
+    this.init()    
 }
-vm({})
+new vm({})
